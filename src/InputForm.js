13fc3af9f3daa05e4cc9122c46b7549cff1dfb4d
Philip Tharakan <tharakan22@gmail.com>
2022-07-04 13:47:31 -0400

    git test

--- src/InputForm.js
+++ src/InputForm.js
@@ -5,19 +5,17 @@ const InputForm = props => {
     props.grabName(e.target.value);
   };
 
-
-  
-  const alertMessage = (e) => {
-    alert(`${props.name()} , welcome to my react app`)
-    console.log(props.name())
-  } 
+  const alertMessage = e => {
+    alert(`${props.name()} , welcome to my react app`);
+    console.log(props.name());
+    console.log("test add from git");
+  };
 
   return (
     <div>
- 
       <form>
         <label>Enter your Name: </label>
-        <input type="text" onChange={changeName}></input>
+        <input type="text" onChange={changeName}></input>s
         <button type="button" onClick={alertMessage}>
           Submit
         </button>
